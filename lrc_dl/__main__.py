375b1760c0c17bcd2dcf53d6bcdcd45cb77ea8f9
SuleimanTheMagnificen <comm.cody2224@gmail.com>
2026-06-11 20:47:58 +0700

    Added several flags

--- lrc_dl/__main__.py
+++ lrc_dl/__main__.py
@@ -9,14 +9,21 @@ def main():
         if args[0].endswith(("opus", "mp3", "wav")):
             client.get_lyrics(sys.argv[1].rstrip(".opus").rstrip(".mp3").rstrip(".wav"))
         elif args[0] == "scan":
-            files: list = client.get_audio_file()
-            for n in files:
-                client.get_lyrics(n)
+            if not client.flag_check(args[1:]):
+                return
+            client.scan_folder(flags=args[1:])
+        elif args[0].startswith("-"):
+            if not client.flag_check(args[0:]):
+                return
+            query: str = input("Search: ")
+            client.get_lyrics(query, flags=args[0:])
         else:
-            client.get_lyrics(args[0])
+            if not client.flag_check(args[1:]):
+                return
+            client.get_lyrics(args[0], flags=args[1:])
     else:
         query: str = input("Search: ")
-        client.get_lyrics(query)
+        client.get_lyrics(query, flags=["-m"])
 
 
 if __name__ == "__main__":
